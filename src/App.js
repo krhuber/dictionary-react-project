f3ea0c475688b4f6c6512a02c92e2355af0d9115
krhuber <kr.huber@gmail.com>
2022-11-01 14:25:52 -0400

    Initial commit - Project setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <img src={logo} className="App-logo img-fluid" alt="logo" />
+    </div>
+  );
+}
+
+export default App;
