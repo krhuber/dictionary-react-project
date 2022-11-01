567a984c129f636649e800b79390cfb3a93869e7
krhuber <kr.huber@gmail.com>
2022-11-01 14:36:58 -0400

    Added Bootstrap
    Bootstrap was not working from npm install so added it via index.html

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header"></header>
       <img src={logo} className="App-logo img-fluid" alt="logo" />
+      <a href="/" className="btn btn-primary">
+        Hello
+      </a>
     </div>
   );
 }
